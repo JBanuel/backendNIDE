4838ff6d18572e1629f4df4972e01198c16106f7
JBanuel <j.manuel.bs@hotmail.com>
2026-04-22 20:25:34 -0600

    correction app logic and code identation

--- app.mjs
+++ app.mjs
@@ -6,10 +6,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//let connection;
+const port = process.env.PORT ?? 8080;
+const ipAddress = process.env.C9_HOSTNAME ?? 'localhost';
+
 let connection;
 
 async function startServer() {
   try {
+    let connection = await db.connect();
     connection = await db.connect();
     console.log("Conectado a Kidplays");
     app.listen(8080, () => console.log("Servidor en puerto 8080"));
@@ -21,6 +26,7 @@ async function startServer() {
 app.post('/login', async (req, res) => {
   const { email, password, nombreRol } = req.body;
   try {
+    let connection = await db.connect();
     const user_loged = await db.loginUser(connection, email, password, nombreRol);
     res.status(200).json(user_loged);
   } catch (err) {
@@ -29,24 +35,26 @@ app.post('/login', async (req, res) => {
 });
 
 app.post('/loginJuego', async (req, res) => {
-    const { id, password } = req.body; 
-    
-    try {
-        const user_loged = await db.loginJuego(connection, id, password);
-        res.status(200).json(user_loged);
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
+  const { id, password } = req.body;
+
+  try {
+    let connection = await db.connect();
+    const user_loged = await db.loginJuego(connection, id, password);
+    res.status(200).json(user_loged);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
 
 app.post('/register', async (req, res) => {
-  const { nombre, apellido, fecha_nacimiento, genero, correo, contrasena, nombreRol} = req.body;
+  const { nombre, apellido, fecha_nacimiento, genero, correo, contrasena, nombreRol } = req.body;
   try {
+    let connection = await db.connect();
     const id_user_created = await db.createUser(connection, nombre, apellido, fecha_nacimiento, genero, correo, contrasena, nombreRol);
     res.status(200).json({
       message: "Registro exitoso",
       id: id_user_created,
-      nombreRol: nombreRol 
+      nombreRol: nombreRol
     });
   } catch (err) {
     res.status(500).json({ error: "No se pudo completar el registro: " + err.message });
@@ -54,35 +62,37 @@ app.post('/register', async (req, res) => {
 });
 
 app.post('/dash/instructor/crearEstudiante', async (req, res) => {
-    const { nombre, apellido, fecha_nacimiento, genero, correo, contrasena,dificultad } = req.body;
+  const { nombre, apellido, fecha_nacimiento, genero, correo, contrasena, dificultad } = req.body;
 
-    try {
-        const id_user_created = await db.createEstudiante(
-            connection, 
-            nombre, 
-            apellido, 
-            fecha_nacimiento, 
-            genero, 
-            correo, 
-            contrasena,  
-            dificultad
-        );
+  try {
+    let connection = await db.connect();
+    const id_user_created = await db.createEstudiante(
+      connection,
+      nombre,
+      apellido,
+      fecha_nacimiento,
+      genero,
+      correo,
+      contrasena,
+      dificultad
+    );
 
-        res.status(200).json({
-            message: "Registro exitoso",
-            id: id_user_created,
-        });
+    res.status(200).json({
+      message: "Registro exitoso",
+      id: id_user_created,
+    });
 
-    } catch (err) {
-        res.status(500).json({ 
-            error: "No se pudo completar el registro: " + err.message 
-        });
-    }
+  } catch (err) {
+    res.status(500).json({
+      error: "No se pudo completar el registro: " + err.message
+    });
+  }
 });
 
 app.post('/juego/addCombate', async (req, res) => {
   const { idEstudiante, idNPC, preguntasHechas, aciertos, duracion, fecha_combate, dificultad } = req.body;
   try {
+    let connection = await db.connect();
     const idCombate = await db.addCombate(connection, idEstudiante, idNPC, preguntasHechas, aciertos, duracion, fecha_combate, dificultad);
     res.status(200).json({
       message: "Registro exitoso",
@@ -99,11 +109,11 @@ app.get('/dash/instructor/asignacionEstudiantes', async (req, res) => {
 
 app.post('/dash/instructor', async (req, res) => {
   const { id_instructor } = req.body;
+
   try {
+    let connection = await db.connect();
     const arrEstadisticas = await db.getEstadisticasEstudiantes(connection, id_instructor);
-    res.status(200).json({
-      estadisticas: arrEstadisticas
-    });
+    res.status(200).json(arrEstadisticas);
   } catch (err) {
     res.status(500).json({ error: "No se pudo completar el registro: " + err.message });
   }
@@ -111,6 +121,7 @@ app.post('/dash/instructor', async (req, res) => {
 
 app.get('/dash/admin/getAllUnauthorized', async (req, res) => {
   try {
+    let connection = await db.connect();
     const arrEstadisticas = await db.getEstadisticasEstudiantes(connection, id_instructor);
     res.status(200).json({
       estadisticas: arrEstadisticas
@@ -120,7 +131,9 @@ app.get('/dash/admin/getAllUnauthorized', async (req, res) => {
   }
 });
 
-if (process.env.NODE_ENV !== 'test' && !process.env.LAMBDA_TASK_ROOT) {
-  app.listen(8080, () => console.log("Servidor local en puerto 8080"));
+if (process.env.AWS_LAMBDA_FUNCTION_NAME === undefined) {
+  app.listen(port, () => {
+    console.log(`Server listening at http://localhost::8080`);
+  });
 }
 export default app;
--- dbManagement.mjs
+++ dbManagement.mjs
@@ -4,37 +4,37 @@ import config from './config.mjs';
 
 export default class dbManagement {
     static async createUser(connection, nombre, apellido, fecha_nacimiento, genero, correo, contrasena, nombreRol) {
-    try {
-        const hash = await bcrypt.hash(contrasena, 7);
+        try {
+            const hash = await bcrypt.hash(contrasena, 7);
 
-        const [rows] = await connection.execute('CALL sp_registrar_usuario(?, ?, ?, ?, ?, ?, ?)', [correo, hash, nombreRol, nombre, apellido, fecha_nacimiento, genero]);
+            const [rows] = await connection.execute('CALL sp_registrar_usuario(?, ?, ?, ?, ?, ?, ?)', [correo, hash, nombreRol, nombre, apellido, fecha_nacimiento, genero]);
 
-        const idUsuario = rows[0][0].idUsuario;
-        
-        return idUsuario;
-    } catch (error) {
-        console.error("Error al crear usuario:", error.message);
-        throw error;
-}
-    
-}
-    static async createEstudiante(connection, nombre, apellido, fecha_nacimiento, genero, correo, contrasena, dificultad) {
-    //const { correo, contrasena, nombre, apellido, fecha_nacimiento, genero, dificultad } = datos;
+            const idUsuario = rows[0][0].idUsuario;
 
-    try {
-        const hash = await bcrypt.hash(contrasena, 7);
+            return idUsuario;
+        } catch (error) {
+            console.error("Error al crear usuario:", error.message);
+            throw error;
+        }
 
-        const [rows] = await connection.execute('CALL sp_registrar_estudiante(?, ?, ?, ?, ?, ?, ?)', [correo, hash, nombre, apellido, fecha_nacimiento, genero, dificultad]);
-
-        const idUsuario = rows[0][0].idUsuario;
-        
-        return idUsuario;
-
-    } catch (err) {
-        console.error("Error al crear el estudiante:", err.message);
-        throw err;
     }
-}
+    static async createEstudiante(connection, nombre, apellido, fecha_nacimiento, genero, correo, contrasena, id_instructor, dificultad) {
+        //const { correo, contrasena, nombre, apellido, fecha_nacimiento, genero, dificultad } = datos;
+
+        try {
+            const hash = await bcrypt.hash(contrasena, 7);
+
+            const [rows] = await connection.execute('CALL sp_registrar_estudiante(?, ?, ?, ?, ?, ?, ?)', [correo, hash, nombre, apellido, fecha_nacimiento, genero, id_instructor, dificultad]);
+
+            const idUsuario = rows[0][0].idUsuario;
+
+            return idUsuario;
+
+        } catch (err) {
+            console.error("Error al crear el estudiante:", err.message);
+            throw err;
+        }
+    }
 
     static async loginUser(connection, correo, contrasena, nombreRol) {
         const query = `
@@ -43,7 +43,7 @@ export default class dbManagement {
             JOIN Usuario_Rol ur ON u.id = ur.id_usuario
             JOIN Rol r ON ur.id_rol = r.id
             WHERE u.correo = ? AND r.rol = ?`;
-        
+
         try {
             const [rows] = await connection.execute(query, [correo, nombreRol]);
             if (rows.length !== 1) throw new Error('Usuario no encontrado');
@@ -61,35 +61,39 @@ export default class dbManagement {
     }
 
     static async loginJuego(connection, id, password) {
-    try {
-        const [results] = await connection.execute('CALL sp_get_datos_login_juego(?)', [id]);
+        try {
+            const [results] = await connection.execute('CALL sp_get_datos_login_juego(?)', [id]);
 
-        const rowsEstudiante = results[0];
-        const rowsNPCs = results[1];
+            const rowsEstudiante = results[0];
+            const rowsNPCs = results[1];
 
-        if (rowsEstudiante.length === 0)  throw new Error('Usuario no encontrado');
+            if (rowsEstudiante.length === 0) throw new Error('Usuario no encontrado');
 
-        const user = rowsEstudiante[0];
+            const user = rowsEstudiante[0];
 
-        const isValid = user.contrasena === password; //await bcrypt.compare(password, user.contrasena);
-        if (!isValid) throw new Error('Contraseña incorrecta');
+            const isValid = user.contrasena === password; //await bcrypt.compare(password, user.contrasena);
+            if (!isValid) throw new Error('Contraseña incorrecta');
 
-        const enemigosDerrotados = rowsNPCs.map(npc => ({id_npc: npc.id_npc, derrotado:npc.derrotado, operacion: npc.operacion, tipo: npc.tipo, nombre: npc.nombre}));
+            const enemigosDerrotados = rowsNPCs.map(npc => ({ id_npc: npc.id_npc, derrotado: npc.derrotado, operacion: npc.operacion, tipo: npc.tipo, nombre: npc.nombre }));
 
-        const { contrasena: _, ...publicUser } = user;
-        
-        return {...publicUser, enemigosDerrotados };
+            const { contrasena: _, ...publicUser } = user;
 
-    } catch (err) {
-        console.error("Error en loginJuego:", err.message);
-        throw err;
+            return { ...publicUser, enemigosDerrotados };
+
+        } catch (err) {
+            console.error("Error en loginJuego:", err.message);
+            throw err;
+        }
     }
-}
 
-    static async addCombate(connection, idEstudiante, idNPC, preguntasHechas, aciertos, duracion, fecha_combate, dificultad) {        
+    static async addCombate(connection, idEstudiante, idNPC, preguntasHechas, aciertos, duracion, fecha_combate, dificultad) {
         try {
             const query = 'INSERT INTO Combate (id_estudiante, id_npc, num_preguntas, aciertos, segundos, fecha, dificultad) VALUES (?, ?, ?, ?, ?, ?, ?)';
             const [resCombate] = await connection.execute(query, [idEstudiante, idNPC, preguntasHechas, aciertos, duracion, fecha_combate, dificultad]);
+            if (aciertos == 5) {
+                const queryDerrotado = 'UPDATE Estudiante_NPC_Derrotado SET derrotado = 1 WHERE id_estudiante = ? AND id_npc = ?;';
+                const [resDerrotado] = await connection.execute(queryDerrotado, [idEstudiante, idNPC]);
+            }
             const idCombate = resCombate.insertId;
             return idCombate;
         } catch (err) {
@@ -98,35 +102,37 @@ export default class dbManagement {
         }
     }
 
-    static async getEstadisticasEstudiantes(connection, idInstructor) {        
+    static async getEstadisticasEstudiantes(connection, idInstructor) {
+        let query = 'CALL sp_obtener_dashboard_instructor(?)';
+        
         try {
-            const query = `
-                SELECT 
-                    p.nombre, 
-                    p.apellido, 
-                    es.monedas,
-                    COALESCE(SUM(co.num_preguntas), 0) AS total_preguntas,
-                    COALESCE(SUM(co.aciertos), 0) AS total_aciertos,
-                    COALESCE(SUM(co.segundos), 0) AS total_segundos,
-                    IF(SUM(co.num_preguntas) > 0, 
-                    ROUND((SUM(co.aciertos) * 100) / SUM(co.num_preguntas), 2), 
-                    0) AS porcentaje_exito
-                FROM Estudiante es
-                INNER JOIN Persona p ON es.id = p.id
-                LEFT JOIN Combate co ON es.id = co.id_estudiante
-                WHERE es.id_instructor = ?
-                GROUP BY es.id, p.nombre, p.apellido, es.monedas
-                ORDER BY porcentaje_exito DESC;
-            `;
-            const [rowsEstadisticas] = await connection.execute(query, [idInstructor]);
-            return rowsEstadisticas;
+            const [results] = await connection.execute(query, [idInstructor]);
+
+            const totalEstudiantes = results[0][0].estudiantes;
+            const listaAlumnos = results[1];
+            const todosLosCombates = results[2];
+
+            const response = {
+            estudiantes: totalEstudiantes,
+            getStudents: listaAlumnos.map(estudiante => ({
+                id: estudiante.id,
+                name: estudiante.name,
+                gender: estudiante.gender,
+                difficulty: estudiante.difficulty,
+                completedNPCs: estudiante.completedNPCs,
+                history: todosLosCombates
+                .filter(c => c.id_estudiante === estudiante.id)
+                .map(({ id_estudiante, ...resto }) => resto)
+            }))
+            };
+
+            return response;
         } catch (err) {
-            console.error("Error en login:", err.message);
             throw err;
         }
     }
-    
-    static async getAllUnauthorized(){
+
+    static async getAllUnauthorized() {
         //const id, name, role, auth
         const query = `SELET id name role auth
         FROM Usuario us
